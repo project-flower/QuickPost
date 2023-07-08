3319ad25b6e7ca13ac1920f1c65147055772fb48
Yuki Nishina <project_flower@outlook.com>
2023-07-08 23:32:11 +0900

    基本レイアウト 作成

---
+++ QuickPost/MainWindow/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using QuickPost.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickPost
+{
+    public class MainWindowViewModel
+    {
+        #region Public Properties
+
+        public string EndPoint { get; set; }
+        public ObservableCollection<PostItem> PostItems { get; set; }
+
+        #endregion
+    }
+}
---
+++ QuickPost/Models/PostItem.cs
@@ -0,0 +1,12 @@
+﻿namespace QuickPost.Models
+{
+    public class PostItem
+    {
+        #region Public Properties
+
+        public string Channel { get; set; }
+        public string Message { get; set; }
+
+        #endregion
+    }
+}
