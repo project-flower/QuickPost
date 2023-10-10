6ec1325a8d9b6b966879b26968f63b6624dacce3
Yuki Nishina <project_flower@outlook.com>
2023-10-11 08:57:32 +0900

    タスクトレイ アイコンをクリックした時に右クリックした時と同じようにコンテキスト メニューが表示されるようにした。

--- QuickPost/Views/FormMain.cs
+++ QuickPost/Views/FormMain.cs
@@ -24,6 +24,7 @@ namespace QuickPost.Views
         private bool isDirty = false;
         private FormWindowState prevWindowState = FormWindowState.Normal;
         private readonly Settings settings = Settings.Default;
+        private readonly MethodInfo? showContextMenuInTaskbar;
 
         #endregion
 
@@ -38,6 +39,9 @@ namespace QuickPost.Views
         public FormMain()
         {
             InitializeComponent();
+
+            // Get protected methods
+            showContextMenuInTaskbar = typeof(ContextMenuStrip).GetMethod("ShowInTaskbar", (BindingFlags.Instance | BindingFlags.NonPublic));
 
             // Set control properties
             notifyIcon.Icon = Icon;
@@ -271,7 +275,16 @@ namespace QuickPost.Views
             {
                 // Suppress display of task tray icon
                 WindowManager.SetForegroundWindow(contextMenuStrip.Handle);
-                contextMenuStrip.Show(Cursor.Position);
+
+                if (showContextMenuInTaskbar != null)
+                {
+                    Point position = Cursor.Position;
+                    showContextMenuInTaskbar.Invoke(contextMenuStrip, new object[] { position.X, position.Y });
+                }
+                else
+                {
+                    contextMenuStrip.Show(Cursor.Position);
+                }
             }
         }
 
